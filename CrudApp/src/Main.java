8dfff1197a370a4e93c5139f26da9019e336d4c6
AllanBill <149832829+AllanBill@users.noreply.github.com>
2025-06-16 20:12:59 -0300

    Update Main.java

--- CrudApp/src/Main.java
+++ CrudApp/src/Main.java
@@ -1,9 +1,16 @@
-
+// Importa a classe TelaPrincipal do pacote de visualização (view)
 import view.TelaPrincipal;
 
+// Classe principal do programa Java
 public class Main {
+
+    // Método principal que inicia a aplicação
     public static void main(String[] args) {
+
+        // Garante que a interface gráfica (Swing) seja criada na thread correta (EDT - Event Dispatch Thread)
         javax.swing.SwingUtilities.invokeLater(() -> {
+
+            // Cria uma nova instância da interface gráfica e a exibe
             new TelaPrincipal().setVisible(true);
         });
     }
